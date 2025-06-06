06bd6766a0ad7c22a5596af084e90096f90d2214
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-06-06 10:10:48 +0530

    fixed error

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const path = require("path");
+
 
 const app = express();
 
