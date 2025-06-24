c9e201c127eb521f24d2cddbb85ebac14f931e26
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-06-24 19:26:08 +0530

    fixed cors issue in frontend url

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ const compression = require("compression");
 
 app.use(
   cors({
-    origin: process.env.ORIGIN_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
 ); 
