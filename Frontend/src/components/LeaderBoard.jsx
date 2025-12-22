9dea1caf3d04b23ed7ff3cb8c9d6dd436ace05c5
Niranjan0524 <niranjan@marketrack.in>
2025-12-22 19:51:50 +0530

    updated leaderboard showing strategy

--- Frontend/src/components/LeaderBoard.jsx
+++ Frontend/src/components/LeaderBoard.jsx
@@ -1,8 +1,31 @@
 import { useLeaderBoard } from "../store/LeaderBoardContext";
-
+import { useState,useEffect } from "react";
+import { FaAngleDoubleDown } from "react-icons/fa";
+import { FaAngleDoubleUp } from "react-icons/fa";
 const Leaderboard = () => {
   const { leaderBoardData } = useLeaderBoard();
 
+  const [splitLeaderBoardData, setSplitLeaderBoardData] = useState(leaderBoardData?.length>6?leaderBoardData.slice(0,6):leaderBoardData);
+
+  const showView=leaderBoardData.length>6;
+  const [viewMore, setViewMore] = useState(false);
+
+  
+  useEffect(()=>{
+    if(leaderBoardData.length>6){
+      if(viewMore){
+        setSplitLeaderBoardData(leaderBoardData);
+      }
+      else{
+        setSplitLeaderBoardData(leaderBoardData.slice(0,6));
+      }
+    }
+  }, [viewMore, leaderBoardData]);
+
+  const handleViewMore=()=>{
+    setViewMore(!viewMore);
+  }
+  
   return (
     <>
       <div className="text-center text-gray-400 mt-12 bg-gradient-to-br from-[#23293a]/60 to-[#1a2233]/80 rounded-xl shadow-lg p-8 border border-[#2a3447] backdrop-blur-md glass-card md:w-1/2 sm:w-1/2 w-full mx-auto">
@@ -27,7 +50,7 @@ const Leaderboard = () => {
                 </tr>
               </thead>
               <tbody>
-                {leaderBoardData.map((user, idx) => (
+                {splitLeaderBoardData.map((user, idx) => (
                   <tr
                     key={user.userId}
                     className={`${
@@ -60,6 +83,15 @@ const Leaderboard = () => {
                 ))}
               </tbody>
             </table>
+            {showView && (
+              <div className="viewMore" onClick={handleViewMore}>
+                {viewMore ?  <span className="flex items-center justify-center mt-4 text-grey-200 cursor-pointer hover:text-grey-900">
+                  View Less <FaAngleDoubleUp className="ml-2" />
+                </span> :  <span className="flex items-center justify-center mt-4 text-grey-200 cursor-pointer hover:text-grey-900">
+                  View More <FaAngleDoubleDown className="ml-2" />
+                  </span>}
+              </div>
+            )}
           </div>
         ) : (
           <p className="text-gray-400 mt-4">No leaderboard data available.</p>
