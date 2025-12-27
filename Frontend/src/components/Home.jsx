3878a7a56222d773e15a6b22238be604d3336a26
Niranjan0524 <parthalase05gmali.com@gmail.com>
2025-12-27 19:59:25 +0530

    testing changes reflecting on github

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -10,10 +10,6 @@ import Problems from "./Problems";
 
 const Home = () => {
 
-  
-
-
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] text-white ">
       {/* Navbar */}
